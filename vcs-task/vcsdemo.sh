00bdfbf6f5e9377f43b53fca21c8a22154d28e10
Sara89s-Git <mail.saravanan23@gmail.com>
2026-03-19 08:31:57 +0530

    second commit for vcs-task

--- vcs-task/vcsdemo.sh
+++ vcs-task/vcsdemo.sh
@@ -3,8 +3,8 @@ echo '-----------------------'
 echo '        VCS demo'
 echo '-----------------------'
 echo ''
-echo 'creating new directory vcs-task'
-mkdir vcs-task
-cd vcs-task
-echo 'inside vc-task'
+echo 'creating new directory vc-task/images'
+mkdir images
+cd images
+echo 'inside vc-task/images'
 echo 'readme'> readme.txt
