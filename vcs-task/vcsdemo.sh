ba7036e26c8bdef09c53ad5c97d3f644fd0293bc
Sara89s-Git <mail.saravanan23@gmail.com>
2026-03-19 08:23:09 +0530

    initial commit

---
+++ vcs-task/vcsdemo.sh
@@ -0,0 +1,10 @@
+
+echo '-----------------------'
+echo '        VCS demo'
+echo '-----------------------'
+echo ''
+echo 'creating new directory vcs-task'
+mkdir vcs-task
+cd vcs-task
+echo 'inside vc-task'
+echo 'readme'> readme.txt
